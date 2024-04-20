0bf58eca39bb27bc13675ce8cdf697bf2947102a
Raul Adriano Ramos <raul.ramos@spassu.com.br>
2024-04-19 23:21:43 -0300

    navegação entre fragments

--- app/src/main/java/com/rauladrianoramos/aulafragment/MainActivity.kt
+++ app/src/main/java/com/rauladrianoramos/aulafragment/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.rauladrianoramos.aulafragment
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.rauladrianoramos.aulafragment.fragments.ChamadasFragment
+import com.rauladrianoramos.aulafragment.fragments.ConversasFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttonConversas : Button
+    private lateinit var buttonChamadas : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+//        val fragmentManager = supportFragmentManager.beginTransaction()
+//
+//        //diversas alteracoes no fragment
+//
+//        fragmentManager.add(R.id.fragment_conteudo, ConversasFragment())
+//
+//        fragmentManager.commit()
+
+        buttonConversas = findViewById(R.id.button_conversas)
+        buttonChamadas = findViewById(R.id.button_chamadas)
+
+        //val conversasFragment = ConversasFragment();
+
+        buttonConversas.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_conteudo,ConversasFragment())
+                .commit()
+        }
+
+        buttonChamadas.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+               .replace(R.id.fragment_conteudo, ChamadasFragment())
+                //.remove(conversasFragment)
+                .commit()
         }
     }
 }
